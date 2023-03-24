ab66dccb3314add4f5d4fc512b0d6468b7508b8c
Juan Burillo <juanburillodev@gmail.com>
2023-03-24 10:48:27 +0100

    JS avanzado

--- js/piano.js
+++ js/piano.js
@@ -1,3 +1,17 @@
+function processChord(tiles) {
+  console.log("hola");
+  let chordCombination = "";
+  for (let i = 0; i < tiles.length; i++) {
+    if (tiles[i]) {
+      chordCombination += i + "_";
+    }
+  }
+
+  chordCombination = chordCombination.slice(0, -1);
+
+  console.log(chordCombination);
+}
+
 function highlightWhiteKey(key, isSelected, sound) {
   if (isSelected) {
     key.style.boxShadow = "none";
@@ -49,81 +63,81 @@ window.onload = function () {
   let whiteTiles = document.getElementsByClassName("white");
   let blackTiles = document.getElementsByClassName("black");
 
-  let tileSelected = new Array(17);
+  let selectedTiles = new Array(17);
   let tileSounds = new Array(17);
   tileSounds = loadSounds(tileSounds);
 
-  for (let i = 0; i < tileSelected.length; i++) {
-    tileSelected[i] = false;
+  for (let i = 0; i < selectedTiles.length; i++) {
+    selectedTiles[i] = false;
   }
 
   // Tile Event Listeners
   whiteTiles[0].addEventListener("click", function () {
-    tileSelected[0] = highlightWhiteKey(whiteTiles[0], tileSelected[0], tileSounds[0]);
+    selectedTiles[0] = highlightWhiteKey(whiteTiles[0], selectedTiles[0], tileSounds[0]);
   });
 
   blackTiles[0].addEventListener("click", function () {
-    tileSelected[1] = highlightBlackKey(blackTiles[0], tileSelected[1], tileSounds[1]);
+    selectedTiles[1] = highlightBlackKey(blackTiles[0], selectedTiles[1], tileSounds[1]);
   });
 
   whiteTiles[1].addEventListener("click", function () {
-    tileSelected[2] = highlightWhiteKey(whiteTiles[1], tileSelected[2], tileSounds[2]);
+    selectedTiles[2] = highlightWhiteKey(whiteTiles[1], selectedTiles[2], tileSounds[2]);
   });
 
   blackTiles[1].addEventListener("click", function () {
-    tileSelected[3] = highlightBlackKey(blackTiles[1], tileSelected[3], tileSounds[3]);
+    selectedTiles[3] = highlightBlackKey(blackTiles[1], selectedTiles[3], tileSounds[3]);
   });
 
   whiteTiles[2].addEventListener("click", function () {
-    tileSelected[4] = highlightWhiteKey(whiteTiles[2], tileSelected[4], tileSounds[4]);
+    selectedTiles[4] = highlightWhiteKey(whiteTiles[2], selectedTiles[4], tileSounds[4]);
   });
 
   blackTiles[2].addEventListener("click", function () {
-    tileSelected[5] = highlightBlackKey(blackTiles[2], tileSelected[5], tileSounds[5]);
+    selectedTiles[5] = highlightBlackKey(blackTiles[2], selectedTiles[5], tileSounds[5]);
   });
 
   whiteTiles[3].addEventListener("click", function () {
-    tileSelected[6] = highlightWhiteKey(whiteTiles[3], tileSelected[6], tileSounds[6]);
+    selectedTiles[6] = highlightWhiteKey(whiteTiles[3], selectedTiles[6], tileSounds[6]);
   });
 
   whiteTiles[4].addEventListener("click", function() {
-    tileSelected[7] = highlightWhiteKey(whiteTiles[4], tileSelected[7], tileSounds[7]);
+    selectedTiles[7] = highlightWhiteKey(whiteTiles[4], selectedTiles[7], tileSounds[7]);
   });
 
   blackTiles[3].addEventListener("click", function () {
-    tileSelected[8] = highlightBlackKey(blackTiles[3], tileSelected[8], tileSounds[8]);
+    selectedTiles[8] = highlightBlackKey(blackTiles[3], selectedTiles[8], tileSounds[8]);
   });
 
   whiteTiles[5].addEventListener("click", function() {
-    tileSelected[9] = highlightWhiteKey(whiteTiles[5], tileSelected[9], tileSounds[9]);
+    selectedTiles[9] = highlightWhiteKey(whiteTiles[5], selectedTiles[9], tileSounds[9]);
   });
 
   blackTiles[4].addEventListener("click", function () {
-    tileSelected[10] = highlightBlackKey(blackTiles[4], tileSelected[10], tileSounds[10]);
+    selectedTiles[10] = highlightBlackKey(blackTiles[4], selectedTiles[10], tileSounds[10]);
   });
 
   whiteTiles[6].addEventListener("click", function() {
-    tileSelected[11] = highlightWhiteKey(whiteTiles[6], tileSelected[11], tileSounds[11]);
+    selectedTiles[11] = highlightWhiteKey(whiteTiles[6], selectedTiles[11], tileSounds[11]);
   });
 
   whiteTiles[7].addEventListener("click", function() {
-    tileSelected[12] = highlightWhiteKey(whiteTiles[7], tileSelected[12], tileSounds[12]);
+    selectedTiles[12] = highlightWhiteKey(whiteTiles[7], selectedTiles[12], tileSounds[12]);
   });
 
   blackTiles[5].addEventListener("click", function () {
-    tileSelected[13] = highlightBlackKey(blackTiles[5], tileSelected[13], tileSounds[13]);
+    selectedTiles[13] = highlightBlackKey(blackTiles[5], selectedTiles[13], tileSounds[13]);
   });
 
   whiteTiles[8].addEventListener("click", function() {
-    tileSelected[14] = highlightWhiteKey(whiteTiles[8], tileSelected[14], tileSounds[14]);
+    selectedTiles[14] = highlightWhiteKey(whiteTiles[8], selectedTiles[14], tileSounds[14]);
   });
 
   blackTiles[6].addEventListener("click", function () {
-    tileSelected[15] = highlightBlackKey(blackTiles[6], tileSelected[15], tileSounds[15]);
+    selectedTiles[15] = highlightBlackKey(blackTiles[6], selectedTiles[15], tileSounds[15]);
   });
 
   whiteTiles[9].addEventListener("click", function() {
-    tileSelected[16] = highlightWhiteKey(whiteTiles[9], tileSelected[16], tileSounds[16]);
+    selectedTiles[16] = highlightWhiteKey(whiteTiles[9], selectedTiles[16], tileSounds[16]);
   });
 
   let noteSwitch = document.getElementsByName("switch")[0];
@@ -138,6 +152,12 @@ window.onload = function () {
         noteNames[i].style.display = "none";
       }
     }
+  });
+
+  let chordButton = document.querySelector("#chordButton");
+
+  chordButton.addEventListener("click", function() {
+    processChord(selectedTiles);
   });
 
 };
